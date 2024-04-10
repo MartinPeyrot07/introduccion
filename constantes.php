3c4ab72135b57b65f7432775c041e9f26348c61f
Martin Peyrot <martin.p23123@gmail.com>
2024-04-10 08:52:50 -0300

    php de constantes, PI

---
+++ constantes.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constantes</title>
+</head>
+<body>
+    <?php 
+    define ('PI' , 3.1416); 
+    define ('PI' , 3.14);
+    ?>    
+
+</body>
+</html>
