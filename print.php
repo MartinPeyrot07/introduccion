c696a879e5c1f04ba1b266b73f3cee8909be06fe
Martin Peyrot <martin.p23123@gmail.com>
2024-04-10 09:13:12 -0300

    php explicativo de var_dump

---
+++ print.php
@@ -0,0 +1,21 @@
+1 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$modeloDeAuto = "Toyota Supra";
+$numeroSerie = 0001;
+$kilometraje = 4617.32;
+var_dump($modeloDeAuto); 
+var_dump($numeroSerie); 
+var_dump($kilometraje); 
+//La utilización del var_dump sirve para obtener la información de una variable
+//En el caso de los String te brinda la variable, junto a la cantidad de caracteres
+//En el caso de los Int y Float, te enseña únicamente el valor de la variable
+?>
+</body>
+</html>
